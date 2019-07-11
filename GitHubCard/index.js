8cfbf4196732d216eec4089c33f3d23cc2b9db87
JCRN <clemons.jason@gmail.com>
2019-07-11 16:41:39 -0700

    mvp ready

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,50 +1,89 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
+// Get the top level element
+const cards = document.querySelector('.cards')
+
+getData('JCRN')
+
+// // // Define an array of follower logins, retrieved from: https://api.github.com/users/JCRN/followers
+const followersArray = ['DeejayEaster', 'fskeen', 'ElvisKnapman', 'pdadlani', 'czclaxton', 'mariam-farrukh', 'Pmtague', 'daredtech', 'arvagas', 'LandryIrakoze', 'JerryOsorio28', 'MarquesJ8023', 'AlexisPanyathong', 'tommycoleman87'];
+
+// Creates a new user card for each follower
+followersArray.forEach(follower => getData(follower))
+
+
+
+/**************************************************************************** FUNCTIONS *************************************************************************/
+
+/* 
+1. Takes github user login as argument 
+2. Axios: requests (usr) data from github API 
+  Success:
+    3. Creates a new user card by Calling createUserCard() and passing (usr) data object   
+    4. Injects new user card into the DOM 
+  Failure:
+    3. Alert message   
 */
+function getData(usr) {
+  axios.get(`https://api.github.com/users/${usr}`)
+  .then(results => {
+    // create usr card
+    console.log(results.data)
+    const usrCard = createCard(results.data)
+    cards.appendChild(usrCard) 
+  })
+  .catch(error => {
+    alert('OOPS! Something Went Wrong :(', error)
+  })
+}
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
 
-   Skip to Step 3.
-*/
 
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
+// Takes data object, creates elements / sets values / returns a github usercard  
+function createCard(data) {
+  // Define elements
+  const card = document.createElement('div')
+  const cardImg = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const cardInfoHeader = document.createElement('h3')
+  const cardInfoUserName = document.createElement('p')
+  const cardInfoLocation = document.createElement('p')
+  const cardInfoProfile = document.createElement('p')
+  const cardInfoProfileURL = document.createElement('a')
+  const cardInfoFollowers = document.createElement('p')
+  const cardInfoFollowing = document.createElement('p')
+  const cardInfoBio = document.createElement('p')
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
+  // Add class names
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  cardInfoHeader.classList.add('name')
+  cardInfoUserName.classList.add('username')
 
-const followersArray = [];
+  // Set content
+  cardImg.src = data.avatar_url
+  cardInfoHeader.textContent = data.name
+  cardInfoUserName.textContent = data.login
+  cardInfoLocation.textContent = `Location: ${data.location}`
+  cardInfoProfileURL.href = `Profile: ${data.html_url}`
+  cardInfoFollowers.textContent = `Followers: ${data.followers}`
+  cardInfoFollowing.textContent = `Following: ${data.following}`
+  cardInfoBio.textContent = `Bio: ${data.bio  }`
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+  // Build the card
+  card.appendChild(cardImg)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(cardInfoHeader)
+  cardInfo.appendChild(cardInfoUserName)
+  cardInfo.appendChild(cardInfoLocation)
+  cardInfo.appendChild(cardInfoLocation)
+  cardInfo.appendChild(cardInfoProfile)
+  cardInfoProfile.appendChild(cardInfoProfileURL)
+  cardInfo.appendChild(cardInfoFollowers)
+  cardInfo.appendChild(cardInfoFollowing)
+  cardInfo.appendChild(cardInfoBio)
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-
-*/
+  // Return completed card
+  return card
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
